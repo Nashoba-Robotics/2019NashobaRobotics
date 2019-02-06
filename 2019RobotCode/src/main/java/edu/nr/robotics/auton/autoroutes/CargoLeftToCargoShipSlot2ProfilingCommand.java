9d4a722aab4f0024a36386ce3e9aacaefacff840
Tyler Wannamaker <tyler.wannamaker@gmail.com>
2019-02-06 18:51:20 -0500

    began adding other subsystems into autos

--- 2019RobotCode/src/main/java/edu/nr/robotics/auton/autoroutes/CargoLeftToCargoShipSlot2ProfilingCommand.java
+++ 2019RobotCode/src/main/java/edu/nr/robotics/auton/autoroutes/CargoLeftToCargoShipSlot2ProfilingCommand.java
@@ -2,14 +2,37 @@ package edu.nr.robotics.auton.autoroutes;
 
 import edu.nr.lib.units.Angle;
 import edu.nr.lib.units.Distance;
+import edu.nr.robotics.Robot;
+import edu.nr.robotics.auton.AutoChoosers.GamePiece;
 import edu.nr.robotics.subsystems.drive.Drive;
 import edu.nr.robotics.subsystems.drive.EnableTwoDMotionProfile;
+import edu.nr.robotics.subsystems.elevator.ElevatorPositionCommand;
+import edu.nr.robotics.subsystems.intakerollers.IntakeRollers;
+import edu.nr.robotics.subsystems.intakerollers.IntakeRollersReverseCommand;
+import edu.nr.robotics.subsystems.elevator.Elevator;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.ConditionalCommand;
 
 public class CargoLeftToCargoShipSlot2ProfilingCommand extends CommandGroup {
 
     public CargoLeftToCargoShipSlot2ProfilingCommand() {
         addSequential(new EnableTwoDMotionProfile(Distance.ZERO, Distance.ZERO, Angle.ZERO, Distance.ZERO, Distance.ZERO, Angle.ZERO, Drive.PROFILE_DRIVE_PERCENT, Drive.accelPercent, "CargoLeftToCargoShipSlot2"));
+    
+        addSequential(new ConditionalCommand(new ElevatorPositionCommand(Elevator.getInstance().CARGO_PLACE_LOW_HEIGHT_ELEVATOR), new ElevatorPositionCommand(Elevator.getInstance().HATCH_PLACE_LOW_HEIGHT_ELEVATOR)) {
+            // first command runs if condition is true, second one runs otherwise 
+ 
+            protected boolean condition() {
+                return Robot.getInstance().selectedGamePiece2 == GamePiece.cargo; // (Boolean) null;
+            }
+
+        });
+
+        addSequential(new ConditionalCommand(new IntakeRollersReverseCommand(IntakeRollers.getInstance().OUTTAKE_PERCENT)){
+        
+            protected boolean condition() {
+                return Robot.getInstance().selectedGamePiece2 == GamePiece.cargo;
+            }
+        });
     }
-;
+
 }
