ddb7468ce98cfaacff465fdc6db8c8714dad9fd9
Tyler Wannamaker <tyler.wannamaker@gmail.com>
2019-02-15 20:29:28 -0500

    sniper mode

--- 2019RobotCode/src/main/java/edu/nr/robotics/subsystems/drive/EnableSniperForwardMode.java
+++ 2019RobotCode/src/main/java/edu/nr/robotics/subsystems/drive/EnableSniperForwardMode.java
@@ -4,17 +4,18 @@ import edu.nr.lib.commandbased.NRCommand;
 
 public class EnableSniperForwardMode extends NRCommand {
 
-	private boolean boo;
+	private boolean bool;
 	
 	public static final double MOVE_JOYSTICK_MULTIPLIER_LOW = 0.5;
 	public static final double MOVE_JOYSTICK_MULTIPLIER_HIGH = 1.0;
-
-	public EnableSniperForwardMode(boolean boo) {
-		this.boo = boo;
+	
+	public EnableSniperForwardMode(boolean bool) {
+		this.bool = bool;
 	}
-
+	
+	@Override
 	protected void onStart() {
-		if (boo) {
+		if (bool) {
 			Drive.MOVE_JOYSTICK_MULTIPLIER = MOVE_JOYSTICK_MULTIPLIER_LOW;
 		} else {
 			Drive.MOVE_JOYSTICK_MULTIPLIER = MOVE_JOYSTICK_MULTIPLIER_HIGH;
--- 2019RobotCode/src/main/java/edu/nr/robotics/subsystems/drive/EnableSniperTurnMode.java
+++ 2019RobotCode/src/main/java/edu/nr/robotics/subsystems/drive/EnableSniperTurnMode.java
@@ -4,22 +4,23 @@ import edu.nr.lib.commandbased.NRCommand;
 
 public class EnableSniperTurnMode extends NRCommand {
 
-	private boolean boo;
-
+	private boolean bool;
+	
 	public static final double TURN_JOYSTICK_MULTIPLIER_LOW = 0.5;
 	public static final double TURN_JOYSTICK_MULTIPLIER_HIGH = 1.0;
-
-	public EnableSniperTurnMode(boolean boo) {
-		this.boo = boo;
+	
+	public EnableSniperTurnMode(boolean bool) {
+		this.bool = bool;
 	}
-
+	
+	@Override
 	protected void onStart() {
-		if (boo) {
+		if (bool) {
 			Drive.TURN_JOYSTICK_MULTIPLIER = TURN_JOYSTICK_MULTIPLIER_LOW;
 		} else {
 			Drive.TURN_JOYSTICK_MULTIPLIER = TURN_JOYSTICK_MULTIPLIER_HIGH;
 		}
-
-		Drive.sniperModeEnabled = boo;
+		
+		Drive.sniperModeEnabled = bool;
 	}
 }
