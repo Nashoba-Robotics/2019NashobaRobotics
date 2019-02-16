01b40b87def95cbfbb608a8ce66e4faafb9aae00
Tyler Wannamaker <tyler.wannamaker@gmail.com>
2019-02-16 16:28:11 -0500

    prepare climb command

---
+++ 2019RobotCode/src/main/java/edu/nr/robotics/multicommands/PrepareClimbCommand.java
@@ -0,0 +1,16 @@
+package edu.nr.robotics.multicommands;
+
+import edu.nr.lib.units.Distance;
+import edu.nr.robotics.subsystems.elevator.ElevatorPositionCommand;
+import edu.nr.robotics.subsystems.intakerollers.IntakeRollersDeployCommand;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class PrepareClimbCommand extends CommandGroup {
+
+    public PrepareClimbCommand(Distance elevHeight) {
+        addSequential(new IntakeRollersDeployCommand());
+
+        addSequential(new ElevatorPositionCommand(elevHeight));
+    }
+ 
+}
