cdaf8667255adaf100bdac801cf1aff9b6633bf3
nathanielcrosby <nathanielcrosby@verizon.net>
2019-03-19 20:15:05 -0400

    changes to lift lock

--- 2019RobotCode/src/main/java/edu/nr/robotics/multicommands/PrepareClimbCommand.java
+++ 2019RobotCode/src/main/java/edu/nr/robotics/multicommands/PrepareClimbCommand.java
@@ -4,9 +4,9 @@ import edu.nr.lib.units.Distance;
 import edu.nr.lib.units.Time;
 import edu.nr.robotics.subsystems.elevator.ElevatorPositionCommand;
 import edu.nr.robotics.subsystems.elevator.ElevatorSwitchToClimbGearCommand;
-import edu.nr.robotics.subsystems.intakerollers.IntakeRollers;
-import edu.nr.robotics.subsystems.intakerollers.IntakeRollersDeployCommand;
+import edu.nr.robotics.subsystems.liftlockmechanism.LiftLockMechanism;
 import edu.nr.robotics.subsystems.liftlockmechanism.LiftLockMechanismDeployCommand;
+import edu.nr.robotics.subsystems.liftlockmechanism.LiftLockMechanismRetractCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
@@ -15,9 +15,9 @@ public class PrepareClimbCommand extends CommandGroup {
     public PrepareClimbCommand(Distance elevHeight) {
         addSequential(new ElevatorPositionCommand(elevHeight));
 
-        addSequential(new IntakeRollersDeployCommand());
+        addSequential(new LiftLockMechanismRetractCommand());
 
-        addSequential(new WaitCommand(IntakeRollers.ACTUATION_TIME.get(Time.Unit.SECOND)));
+        addSequential(new WaitCommand(LiftLockMechanism.ACTUATION_TIME.get(Time.Unit.SECOND)));
 
         addSequential(new LiftLockMechanismDeployCommand());
 
